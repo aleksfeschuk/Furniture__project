e3462cd85756858762fc6aa6fce28356066cac17
aleksfeschuk <aleksfeschuk@gmail.com>
2026-01-25 00:37:31 -0700

    working with script.js for burger menu (@media) and fixed code and bugs in header.scss

--- js/script.js
+++ js/script.js
@@ -11,24 +11,60 @@ function load() {
 		// Додавання data-fls-touch для HTML, якщо браузер мобільний
 		if (isMobile.any()) document.documentElement.setAttribute('data-fls-touch', '')
 	}
+
+	function initSubMenu() {
+		const matchMedia = window.matchMedia(`(width <= 41.875em)`)
+		const subMenu  = document.querySelector('.sub-menu');
+		matchMedia.addEventListener('change', function() {
+			setSubMenu(matchMedia.matches);
+		})
+		setSubMenu(matchMedia.matches)
+
+		function setSubMenu() {
+			if(matchMedia.matches) {
+				subMenu.style.cssText += `height: 0;`
+			} else  {
+				subMenu.style.cssText = ``;
+			}
+		}
+	}
+
+	
+
+	
 	addTouchAttr()
+	initSubMenu()
 
 	document.addEventListener('click', documentActions)
 
 	function documentActions(e) {
-		
 		const targetElement = e.target
-
 		if (isMobile.any()) {
 			if (targetElement.closest('.menu__sub-link')) {
-			const currentElement = targetElement.closest('.menu__sub-link')
-			document.documentElement.toggleAttribute('data-sub-menu-open')
+				const subMenu = document.querySelector('.sub-menu');
+				document.documentElement.toggleAttribute('data-sub-menu-open')
+				if (window.innerWidth <= 670) {
+					if (document.documentElement.hasAttribute('data-sub-menu-open')) {
+						//open
+						subMenu.style.cssText = ``;
+						const subMenuHeight = subMenu.offsetHeight;
+						subMenu.style.cssText += `height: 0`;
+						subMenu.offsetHeight;
+						subMenu.style.cssText = `height: ${subMenuHeight}px`;
+					} else {
+						 //close
+
+						subMenu.style.cssText += `height: 0`;
+					}
+				}
 			} else {
 				document.documentElement.removeAttribute('data-sub-menu-open')
 			}
 		} 
-		
-		
+		if (targetElement.closest('.icon-menu')) {
+			document.documentElement.toggleAttribute('data-menu-open')
+
+		}
 	}
 
 }
